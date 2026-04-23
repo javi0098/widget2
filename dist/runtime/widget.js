549ea311c0cf8c67edc9f6943cef9367558bc59a
javi0098 <35930014+javi0098@users.noreply.github.com>
2026-04-23 02:17:02 -0300

    Update widget.js

--- dist/runtime/widget.js
+++ dist/runtime/widget.js
@@ -1,11 +1,26 @@
 define([
   'jimu-core',
-  'react'
-], function (jimuCore, React) {
+  'jimu-core/react',
+  'jimu-ui'
+], function (jimuCore, React, jimuUI) {
 
   const { React: JimuReact } = jimuCore;
+  const { Button } = jimuUI;
 
   return class Widget extends React.PureComponent {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        count: 0
+      };
+    }
+
+    onClick = () => {
+      this.setState({
+        count: this.state.count + 1
+      });
+    };
 
     render() {
       return JimuReact.createElement(
@@ -15,15 +30,38 @@ define([
             width: '100%',
             height: '100%',
             display: 'flex',
+            flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
-            fontSize: '20px'
+            gap: '10px'
           }
         },
-        'Hola mundo desde Experience Builder 👋'
+
+        // 👋 TEXTO PRINCIPAL
+        JimuReact.createElement(
+          'h2',
+          null,
+          'Hola mundo desde Experience Builder 👋'
+        ),
+
+        // 🔢 CONTADOR
+        JimuReact.createElement(
+          'p',
+          null,
+          'Clicks: ' + this.state.count
+        ),
+
+        // 🔘 BOTÓN UI de jimu-ui
+        JimuReact.createElement(
+          Button,
+          {
+            onClick: this.onClick,
+            type: 'primary'
+          },
+          'Incrementar'
+        )
       );
     }
-
   };
 
 });
